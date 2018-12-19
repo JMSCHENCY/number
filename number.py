81b0faa78d21734fd253705c448689c05ea89526
James Chen <jmschency@gmail.com>
2018-12-19 19:51:09 +0800

    Add new file

---
+++ number.py
@@ -0,0 +1,11 @@
+# 整數
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+result = int(num1) + int(num2)
+print(result)
+
+# 浮點數
+num3 = input("Enter a number: ")
+num4 = input("Enter another number: ")
+result = float(num3) + float(num4)
+print(result)
